cad13f694f7ee682b41f6cd8e230f814d5618dff
jcalabing <jcalabing@gmail.com>
2020-11-16 10:53:07 +0800

    Updates

---
+++ precache-manifest.8e478bb7791316c006afac85b6abc0a7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4160cb9271469e5d47391a2eed9e44b3",
+    "url": "/pcafront/index.html"
+  },
+  {
+    "revision": "9bc2165cf5c6a277e87f",
+    "url": "/pcafront/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "7e53e56ba0a7a4e5ee31",
+    "url": "/pcafront/static/css/main.8e9f8467.chunk.css"
+  },
+  {
+    "revision": "9bc2165cf5c6a277e87f",
+    "url": "/pcafront/static/js/2.b0ce8204.chunk.js"
+  },
+  {
+    "revision": "34491c6de1fbcf2388878b87785c74e7",
+    "url": "/pcafront/static/js/2.b0ce8204.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e53e56ba0a7a4e5ee31",
+    "url": "/pcafront/static/js/main.8895a3e5.chunk.js"
+  },
+  {
+    "revision": "f7bdf0cfc9ea0dd44285692948c2d84f",
+    "url": "/pcafront/static/js/main.8895a3e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1668441d3f63dd71d2d",
+    "url": "/pcafront/static/js/runtime-main.38ea98df.js"
+  },
+  {
+    "revision": "9eb4c907509e63f20de888cf7aee7318",
+    "url": "/pcafront/static/media/loginbg.9eb4c907.jpg"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/pcafront/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "f86e734309baa8497e17519b3208f3d6",
+    "url": "/pcafront/static/media/pcalogo.f86e7343.png"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/pcafront/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/pcafront/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/pcafront/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/pcafront/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
